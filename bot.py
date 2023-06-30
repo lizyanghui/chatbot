9b6f59da9b3a4ccce0f7d85febdeacd758ec0f75
Hui Yang <lizyanghui@gmail.com>
2023-06-30 11:00:02 -0400

    used ListTrainer to add in customized conversations

--- bot.py
+++ bot.py
@@ -1,6 +1,17 @@
 from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
 
 chatbot = ChatBot('Chatpot')
+
+trainer = ListTrainer(chatbot)
+trainer.train([
+    'Hi',
+    'Welcom, friend 🤗',
+])
+trainer.train([
+    'Are you a plant?',
+    "No, I'm the pot below the plant!",
+])
 
 exit_conditions = (':q', 'quit', 'exit')
 while True:
